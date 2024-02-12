c0a5771db5a479abcb996598a538d627d826d320
SukritiR29 <sukritirajora@gmail.com>
2024-02-12 21:16:58 +0530

    about

--- components/About.jsx
+++ components/About.jsx
@@ -4,19 +4,19 @@ export default function About() {
   return (
   <div>
    <div className="stick lg:ml-20 lg:mr-20 ">
-    <div className="m-3">
-    <h2 className="text-3xl font-semibold text-base-100 uppercase underline">About Me</h2>
-    <a href="http://resume-sukritir29.vercel.app/" className="btn btn-secondary btn-sm mt-5 text-sm pl-14 pr-14 font-normal">
+    <div className="mt-3 mb-3">
+    <h2 className="text-xl font-semibold uppercase underline">About Me</h2>
+    <a href="http://resume-sukritir29.vercel.app/" className="btn btn-base-100 btn-sm mt-5 text-sm pl-14 pr-14 font-normal">
             Resume
           </a>
     </div>
-    <p className="mt-5 ml-4 mr-4 mb-5 text-base-100 text-md text-justify">A programmer with an ardent passion for Web Developing and Data Structures. To develop my talents & knowledge and learn more about my subject of study, I have worked on a numerous impactful projects, several internships & hackathons. I continue to fervently learn about relevant topics while actively working and learning in the field of Full-Stack Development, and its derivatives.</p>
+    <p className="mt-5 ml-4 mr-4 mb-5  text-sm text-justify">A programmer with an ardent passion for Web Developing and Data Structures. To develop my talents & knowledge and learn more about my subject of study, I have worked on a numerous impactful projects, several internships & hackathons. I continue to fervently learn about relevant topics while actively working and learning in the field of Full-Stack Development, and its derivatives.</p>
    </div>
 
-    <div className="lg:m-20 lg:mb-10 lg:mt-5  mr-3 ml-3">
-      <h2  className="text-xl font-semibold text-base-100 uppercase underline">Experience</h2>
+    <div className="lg:m-20 lg:mb-10 lg:mt-10  mr-3 ml-3">
+      <h2  className="text-xl font-semibold  uppercase underline">Experience</h2>
       <div className="lg:flex gap-20">
-      <div className="bg-secondary lg:w-80 mt-5 rounded-md">
+      <div className="bg-accent lg:w-80 mt-10 rounded-md">
         <div className="flex flex-col items-center justify-center pb-4 pt-3 ">
         <h2 className="text-slate-300"> SDE Intern </h2>
         <h5 className="text-slate-300 text-sm"> Quinbay Technology - Bangalore </h5>
@@ -25,8 +25,8 @@ export default function About() {
         </div>
       </div>
 
-      <div className="bg-secondary lg:w-80 mt-5 rounded-md">
-        <div className="flex flex-col items-center justify-center pb-4 pt-3 mb-5 ">
+      <div className="bg-accent lg:w-80 mt-10 rounded-md">
+        <div className="flex flex-col items-center justify-center pb-4 pt-3 mb-1 ">
         <h2 className="text-slate-300"> SDE Intern </h2>
         <h5 className="text-slate-300 text-sm"> MiniOrange - Pune </h5>
         <p className="text-xs">June 2022 - July 2022</p>
@@ -38,9 +38,9 @@ and ethers.js. Implemented a one-click login flow. Used nonce and JWT to authent
 
     </div>
     <div className="lg:m-20 lg:mt-5 mr-3 ml-3">
-      <h2  className="text-xl font-semibold text-base-100 uppercase underline">What I'm Doing</h2>
+      <h2  className="text-xl font-semibold uppercase underline">What I'm Doing</h2>
       <div className="lg:flex gap-20">
-      <div className="bg-secondary lg:w-80 mt-5 rounded-md">
+      <div className="bg-accent lg:w-80 mt-5 rounded-md">
         <div className="flex flex-col items-center justify-center pt-5 ">
         <h2 className="text-slate-300"> Projects </h2>
         
@@ -48,7 +48,7 @@ and ethers.js. Implemented a one-click login flow. Used nonce and JWT to authent
         </div>
       </div>
 
-      <div className="bg-secondary lg:w-80 mt-5 rounded-md">
+      <div className="bg-accent lg:w-80 mt-5 rounded-md">
         <div className="flex flex-col items-center justify-center pb-4 pt-5 ">
         <h2 className="text-slate-300"> Contrubutions </h2>
         <p className="text-xs mt-2 text-justify pb-2 pl-3 pr-3 text-slate-400 font-light">Contributing to the community with Google Developers Students CLub as a UI Lead for 2 years. Conducting workshops on UI UX development and related challenges.</p>
@@ -58,7 +58,7 @@ and ethers.js. Implemented a one-click login flow. Used nonce and JWT to authent
     </div>
     <div className="lg:m-20 lg:mt-0 mr-3 ml-3">
     <div className="lg:flex gap-20">
-      <div className="bg-secondary lg:w-80 rounded-md">
+      <div className="bg-accent lg:w-80 rounded-md">
         <div className="flex flex-col items-center justify-center pt-5 ">
         <h2 className="text-slate-300"> Hackathons </h2>
         
@@ -66,7 +66,7 @@ and ethers.js. Implemented a one-click login flow. Used nonce and JWT to authent
         </div>
       </div>
 
-      <div className="bg-secondary lg:w-80 rounded-md">
+      <div className="bg-accent lg:w-80 rounded-md">
         <div className="flex flex-col items-center justify-center pb-4 pt-5 ">
         <h2 className="text-slate-300"> Reading </h2>
         <p className="text-xs mt-2 text-justify pb-2 pl-3 pr-3 text-slate-400 font-light">When I'm not coding, you'll find me immersed in books, mainly novels. Got any suggestions? Throw them this way!!</p>
